fc075b4797315025f24730815317230cba987646
ferhat <fyildirim@roc-dev.com>
2015-08-19 14:39:46 +0200

    Opdracht 2.1

---
+++ src/h01/Show.java
@@ -0,0 +1,20 @@
+package h01;
+import java.awt.*;
+import java.applet.*;
+
+public class Show extends Applet {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public void init() {
+    setBackground(Color.cyan);
+  }
+
+  public void paint(Graphics g) {
+    g.setColor(Color.yellow);
+    g.drawString("Ferhat Yildirim", 60, 60 );
+  }
+}
